88dc68809e89a1c24030189760a749deb27614bf
Gabriel Duarte <gabrield.developer@gmail.com>
2025-04-28 22:45:19 -0300

    feat: @track properties

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,10 +1,22 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
   greeting = 'World';
   title = 'LWC is awesome,';
 
+  @track address = {
+    city: 'San Francisco',
+    postcode: 6969,
+    country: 'USA'
+  }
+
+  @track list = ['a', 'b', 'c'];
+
   inputHandler(event) {
     this.title = event.target.value;
+  }
+
+  trackHandler(event) {
+    this.address.city = event.target.value;
   }
 }
