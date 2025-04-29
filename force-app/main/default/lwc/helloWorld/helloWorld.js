85ddbc075473c823fb0c0f4e298692c4b388c950
Gabriel Duarte <gabrield.developer@gmail.com>
2025-04-28 22:25:40 -0300

    feat: handlers, methods, LWC Components, LWC Design System usage, two way databindings

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -2,4 +2,9 @@ import { LightningElement } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
   greeting = 'World';
+  title = 'LWC is awesome,';
+
+  inputHandler(event) {
+    this.title = event.target.value;
+  }
 }
