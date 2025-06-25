88d96f1ffac453b4cd565ae9b9e0995853adb3b4
Gabriel Duarte <gabrield.developer@gmail.com>
2025-06-24 23:02:10 -0300

    feat: parent to child non primitive data communication

--- force-app/main/default/lwc/p2cParent/p2cParent.js
+++ force-app/main/default/lwc/p2cParent/p2cParent.js
@@ -1,3 +1,21 @@
 import { LightningElement } from 'lwc';
 
-export default class P2cParent extends LightningElement { }
+export default class P2cParent extends LightningElement {
+  carouselData = [
+    {
+      header: "First card",
+      description: "first card description",
+      href: "https://www.google.com"
+    },
+    {
+      header: "Second card",
+      description: "second card description",
+      href: "https://www.google.com"
+    },
+    {
+      header: "third card",
+      description: "third card description",
+      href: "https://www.google.com"
+    }
+  ]
+}
